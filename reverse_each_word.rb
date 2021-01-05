a8174deb6e487a17289297674082de4fe7c77acf
Matthew Thomas <mzthomas96@gmail.com>
2021-01-05 02:14:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
+  reversedword = sentence.split(" ").collect do |word|
   word.reverse
 end
-  reversed.join(" ")
+  reversedword.join(" ")
 end
